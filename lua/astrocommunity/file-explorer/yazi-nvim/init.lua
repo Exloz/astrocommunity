05af1aab4dd2182b858d542373e79b3683dad3d4
Exloz <exloz@hotmail.com>
2025-12-26 00:29:54 -0500

    feat: add yazi

---
+++ lua/astrocommunity/file-explorer/yazi-nvim/init.lua
@@ -0,0 +1,35 @@
+return {
+  "mikavilpas/yazi.nvim",
+  version = "*",
+  cmd = "Yazi",
+  dependencies = {
+    { "nvim-lua/plenary.nvim", lazy = true },
+    {
+      "AstroNvim/astrocore",
+      ---@type AstroCoreOpts
+      opts = {
+        mappings = {
+          n = {
+            ["<Leader>-"] = { "<Cmd>Yazi<CR>", desc = "Open yazi at the current file" },
+            ["<Leader>Yc"] = { "<Cmd>Yazi cwd<CR>", desc = "Open yazi in nvim's working directory" },
+            ["<Leader>Yt"] = { "<Cmd>Yazi toggle<CR>", desc = "Resume the last yazi session" },
+          },
+          v = {
+            ["<Leader>-"] = { "<Cmd>Yazi<CR>", desc = "Open yazi at the current file" },
+          },
+        },
+      },
+    },
+  },
+  opts = {
+    open_for_directories = false,
+    keymaps = {
+      show_help = "<F1>",
+    },
+    integrations = {
+      grep_in_directory = "snacks.picker",
+      grep_in_selected_files = "snacks.picker",
+    },
+  },
+  init = function() vim.g.loaded_netrwPlugin = 1 end,
+}
